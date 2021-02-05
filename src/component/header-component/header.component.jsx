ec5057ba44ce82e0f482fb9e3c860cfac89f0a85
unknown <abraham_jeffy@yahoo.com>
2021-02-05 18:29:04 +0000

    user avatar added

--- src/component/header-component/header.component.jsx
+++ src/component/header-component/header.component.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import CustomButton from "../custom-button-component/custombutton.component";
 import UserAvatar from "../user-avatar-component/user-avatar.component";
-import './header.styles.css'
-const Header = ({user}) => {
-    {console.log()}
+import "./header.styles.css";
+const Header = ({ user }) => {
+  {
+    console.log();
+  }
   return (
     <div className="navbar-container">
       <div className="navbar-container-flex">
@@ -13,16 +15,8 @@ const Header = ({user}) => {
         <div>
           <CustomButton>CURRENT EXCHANGE RATES</CustomButton>
         </div>
-        {user
-        ? <UserAvatar />
-        : <div>Sign Out</div>}
       </div>
-     
-      <div>
-     
-      </div>
-
-       
+      <div>{user ? <UserAvatar userName={user} /> : <div>Sign Out</div>}</div>
     </div>
   );
 };
--- src/component/user-avatar-component/user-avatar.component.js
+++ src/component/user-avatar-component/user-avatar.component.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import './user-avatar.styles.css'
-const UserAvatar = () => {
-  return (<div>
-      <div >
-        Jeffy Abraham
-      </div>
-  </div>);
+import React from "react";
+import "./user-avatar.styles.css";
+const UserAvatar = ({ userName }) => {
+  return (
+    <div className='user-avatar-container'>
+      <button className='user-avatar'>
+        <span>J A</span>
+      </button>
+      <span>Jeffy Abraham</span>
+    </div>
+  );
 };
 
 export default UserAvatar;
